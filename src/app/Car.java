1c0cbf6ce5512bf6f296fd78f0ce4bf2cac6d5d7
Ganesh Yadav <Ganesh Yadav@192.168.1.16>
2025-11-12 21:06:32 +0530

    Added Car project with class and its executable class

---
+++ src/app/Car.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Car {
+public int speed;
+public String name;
+public String model;
+public String customer;
+void ganesh()
+{
+    System.out.println("speed="+speed);
+	System.out.println("name="+name);
+	System.out.println("model="+model);
+	System.out.println("customer="+customer);
+	
+}
+
+
+
+}
---
+++ src/app/Ganesh.java
@@ -0,0 +1,19 @@
+package app;
+
+public class Ganesh {
+
+	public static void main(String[] args) {
+		Car c= new Car();
+		c.speed=120;
+		c.model="swift";
+		c.name="ganesh";
+		c.customer="ramesh";
+		c.ganesh();
+		Car u= new Car();
+		u.speed=130;
+		u.name="mukesh";
+		u.ganesh();
+
+	}
+
+}

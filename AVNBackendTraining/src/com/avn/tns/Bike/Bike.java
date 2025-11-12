95beacad14d8b143e6ba6716a2238b3016989708
Ganesh Yadav <Ganesh Yadav@192.168.1.16>
2025-11-12 21:31:50 +0530

    Created Package AVNBackendTraining and added code

---
+++ AVNBackendTraining/src/com/avn/tns/Bike/Bike.java
@@ -0,0 +1,18 @@
+package com.avn.tns.Bike;
+
+
+public class Bike{ 
+public int price;
+public float time;
+public String model;
+public String name;
+void ganesh() {
+	System.out.println("price="+price);
+	System.out.println("time="+time);
+	System.out.println("model="+model);
+	System.out.println("name="+name);
+	
+}
+
+
+}
---
+++ AVNBackendTraining/src/com/avn/tns/Bike/BikeMain.java
@@ -0,0 +1,13 @@
+package com.avn.tns.Bike;
+
+public class BikeMain {
+	public static void main(String[] args) {
+		Bike b= new Bike();
+		b.model="BS6";
+		b.name="pulsar";
+		b.price=120000;
+		b.time=11;
+		b.ganesh();
+	}
+
+}
